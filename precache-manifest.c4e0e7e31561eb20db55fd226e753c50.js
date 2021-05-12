39eb9d5b33073199109e3f97c4945a98649966a3
feruz111 <feruznazaroff111@gmail.com>
2021-05-12 15:13:28 +0100

    Updates

---
+++ precache-manifest.c4e0e7e31561eb20db55fd226e753c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1368194637decb7ba76d2eabd018d514",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "c96c3f662ce6d7e6bec3",
+    "url": "/todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a52fe536145ab983f11c",
+    "url": "/todo-list/static/js/2.7a437e90.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todo-list/static/js/2.7a437e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96c3f662ce6d7e6bec3",
+    "url": "/todo-list/static/js/main.8ddf4b0e.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
